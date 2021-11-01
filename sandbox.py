3b72ded0d0c09aa06bfde070e22dad2134b738f2
Kosta Derabin <kostaderabin@gmail.com>
2021-11-01 12:41:57 +0200

    my second update: - added new sandbox py file

---
+++ sandbox.py
@@ -0,0 +1,19 @@
+# Variables in python
+# Variables we can change
+
+x = 34
+y = "Martina Glagow"
+z = "Usche Diesel"
+
+# Boolean variables
+flag1 = True
+flag2 = False
+
+# Comparisons - the building block of logic
+print(f"x = {x}")
+print(f"y = {y}")
+
+print(f"Equal: {x == y}")
+print(f"Not Equal: {x != y}")
+
+print(f"Greater than: {z > y}")
